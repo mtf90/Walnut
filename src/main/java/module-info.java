b212af486a4a6c39998db860f02285be7e6f6a1b
Markus Frohme <markus.frohme@udo.edu>
2026-06-01 22:40:41 +0200

    add dist profile in CI pipeline

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module io.github.walnut_theorem_prover.walnut {
+
+    requires automaton;
+    requires ch.qos.logback.classic;
+    requires ch.qos.logback.core;
+    requires io.github.jn1z.otf;
+    requires it.unimi.dsi.fastutil;
+    requires net.automatalib.api;
+    requires net.automatalib.core;
+    requires net.automatalib.serialization.ba;
+    requires org.slf4j;
+
+}
